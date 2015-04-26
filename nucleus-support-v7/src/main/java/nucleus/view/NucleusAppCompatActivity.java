21d4bb4d042344a5d996b3cb040fe27b7aa36dbe
deividi <deividi.cavarzan@gmail.com>
2015-04-26 17:54:36 -0300

    minVersion at 14 and update support library to 22.1.0, using AppCompatActivity

---
+++ nucleus-support-v7/src/main/java/nucleus/view/NucleusAppCompatActivity.java
@@ -0,0 +1,71 @@
+package nucleus.view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import nucleus.presenter.Presenter;
+
+/**
+ * This class is an example of how an activity could controls it's presenter.
+ * You can inherit from this class or copy/paste this class's code to
+ * create your own view implementation.
+ *
+ * @param <PresenterType> a type of presenter to return with {@link #getPresenter}.
+ */
+public abstract class NucleusAppCompatActivity<PresenterType extends Presenter> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        helper.requestPresenter(getClass(), savedInstanceState == null ? null : savedInstanceState.getBundle(PRESENTER_STATE_KEY));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (isFinishing())
+            destroyPresenter();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBundle(PRESENTER_STATE_KEY, helper.savePresenter());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        helper.takeView(this, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        helper.dropView();
+    }
+
+    // The following section can be copy & pasted into any View class, just update their description if needed.
+
+    /**
+     * Returns a current attached presenter.
+     * This method is guaranteed to return a non-null value between
+     * onResume/onPause calls.
+     *
+     * @return a current attached presenter or null.
+     */
+    public PresenterType getPresenter() {
+        return helper.getPresenter();
+    }
+
+    /**
+     * Destroys a presenter that is currently attached to the View.
+     */
+    public void destroyPresenter() {
+        helper.destroyPresenter();
+    }
+
+    private static final String PRESENTER_STATE_KEY = "presenter_state";
+    private PresenterHelper<PresenterType> helper = new PresenterHelper<>();
+}
